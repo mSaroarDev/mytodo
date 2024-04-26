2ccd8c56bddd1185f20c54d9c8a50ebb44034f15
unknown <msaroar.dev@gmail.com>
2024-04-27 01:51:20 +0600

    base url changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://todo.digiskill-institute.com",
+  base: "https://mytodo24.vercel.app",
 });
